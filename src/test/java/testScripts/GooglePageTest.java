baebd34e868619dd30d595e1326735acd7057130
kanak-1 <kanakgupta9515@gmail.com>
2023-08-09 06:39:16 +0000

    Initial Project

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,21 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePageTest {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.google.com/");
+		WebElement srcBox=driver.findElement(By.id("APjFqb"));
+		srcBox.sendKeys("Java Tutorial");
+		srcBox.sendKeys(Keys.ENTER);
+		String strTitle=driver.getTitle();
+		System.out.println("Page Title-" + strTitle );
+	}
+
+}
