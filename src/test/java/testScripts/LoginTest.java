36fec5f04c9dc4ff7c4405a90d9240406ac3d39a
kanak-1 <141715698+kanak-1@users.noreply.github.com>
2023-08-12 08:10:54 +0000

    Update LoginTest.java
    update

--- src/test/java/testScripts/LoginTest.java
+++ src/test/java/testScripts/LoginTest.java
@@ -9,12 +9,19 @@ public static void main(String[] args) {
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://the-internet.herokuapp.com/login");
-	driver.findElement(By.id("username")).sendKeys("tomsmith");
-	driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+	//driver.findElement(By.id("username")).sendKeys("tomsmith");
+	//driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
 	//driver.findElement(By.className("radius")).click();
-	driver.findElement(By.tagName("button")).click();
+	//driver.findElement(By.tagName("button")).click();
+	//USING xpath
+	//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
+	driver.findElement(By.xpath("//input[@id='username' and @type='text']")).sendKeys("tomsmith");
+	driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword!");
+	driver.findElement(By.xpath("//button[@class='radius']")).click();
+	//driver.findElement(By.xpath("//i[@class='fa fa-2x fa-sign-in']")).click();
 	//driver.findElement(By.linkText("Elemental Selenium")).click();
 	//driver.findElement(By.partialLinkText("Selenium")).click();
-	
+	//Absolute xpath of user name input field=/html/body/div[2]/div/div/form/div[1]/div/input
+	// Absolute xpath of button =/html/body/div[2]/div/div/form/button/i
 }
 }
