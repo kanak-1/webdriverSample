e87fa06f6994000fe70b201e7f0c00ac8d886cd4
kanak-1 <kanakgupta9515@gmail.com>
2023-08-12 06:27:42 +0000

    new commit of LoginTest

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -10,21 +10,29 @@ public class GooglePageTest {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();	
 		driver.manage().window().maximize();
-//		driver.navigate().to("https://www.google.com/");
-//		WebElement srcBox=driver.findElement(By.id("APjFqb"));
-//		srcBox.sendKeys("Java Tutorial Advance");
-//		srcBox.sendKeys(Keys.ENTER);
-//		String strTitle=driver.getTitle();
-//		System.out.println("Page Title-" + strTitle );
-		driver.navigate().to("https://the-internet.herokuapp.com/login");
+		//driver.get("https://www.google.com/");		
+		driver.navigate().to("https://www.google.com/");
+		//WebElement srcBox=driver.findElement(By.id("APjFqb"));
+		//WebElement srcBox=driver.findElement(By.name("q"));
+		//WebElement srcBox=driver.findElement(By.tagName("textarea"));
+		WebElement srcBox=driver.findElement(By.className("gLFyf"));
+		srcBox.sendKeys("Java Tutorial");
+		srcBox.sendKeys(Keys.ENTER);
+		String strTitle=driver.getTitle();
+		System.out.println("Page Title-" + strTitle );
+		System.out.println("Page URL"+ driver.getCurrentUrl() );
+		driver.navigate().back();
+		//driver.navigate().forward();
+		System.out.println("Page Title-" + driver.getTitle());
+		//driver.navigate().to("https://the-internet.herokuapp.com/login");
 	//driver.findElement(By.id("username")).sendKeys("tomsmith");
-		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
+		//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
 		//driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("SuperSecretPassword!");
+		//driver.findElement(By.xpath("//input[@name='password']")).sendKeys("SuperSecretPassword!");
 		//driver.findElement(By.className("radius")).click();
-		driver.findElement(By.xpath("//button[@class='radius']")).click();
+		//driver.findElement(By.xpath("//button[@class='radius']")).click();
 		
-		
+		//driver.close();
 		
 	}
 
---
+++ src/test/java/testScripts/LoginTest.java
@@ -0,0 +1,15 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://the-internet.herokuapp.com/login");
+	//driver.findElement(By.linkText("Elemental Selenium")).click();
+	driver.findElement(By.partialLinkText("Selenium")).click();
+}
+}
