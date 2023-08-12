468675ca4a81d703c679c87883084eebf5dc5db9
kanak-1 <kanakgupta9515@gmail.com>
2023-08-12 06:38:05 +0000

    my token

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GooglePageTest {
 	public static void main(String[] args) {
+		//my token = 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
 		WebDriver driver=new ChromeDriver();	
 		driver.manage().window().maximize();
 		//driver.get("https://www.google.com/");		
