3b8854b928b02ef5b1b426251a04f6b3af54e5a5
kanak-1 <kanakgupta9515@gmail.com>
2023-08-13 07:39:53 +0000

    find elements method

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -1,5 +1,8 @@
 package testScripts;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -7,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GooglePageTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		//my token = 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
 		//my new token for practice=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
 		WebDriver driver=new ChromeDriver();	
@@ -19,11 +22,21 @@ public class GooglePageTest {
 		//WebElement srcBox=driver.findElement(By.tagName("textarea"));
 		WebElement srcBox=driver.findElement(By.className("gLFyf"));
 		srcBox.sendKeys("Java Tutorial");
-		srcBox.sendKeys(Keys.ENTER);
-		String strTitle=driver.getTitle();
-		System.out.println("Page Title-" + strTitle );
-		System.out.println("Page URL"+ driver.getCurrentUrl() );
-		driver.navigate().back();
+		Thread.sleep(2000);
+		List<WebElement> items=driver.findElements(By.xpath("(//ul[@class='G43f7e'])[1]//li//descendant::div[@class='wM6W7d']"));
+		System.out.println("No. of items=" + items.size());
+		for(int i=0;i<items.size();i++) {
+			System.out.println(items.get(i).getText());
+			if(items.get(i).getText().equalsIgnoreCase("kanak")){
+				items.get(i).click();
+				break;
+			}
+		}
+		//srcBox.sendKeys(Keys.ENTER);
+		//String strTitle=driver.getTitle();
+		//System.out.println("Page Title-" + strTitle );
+		//System.out.println("Page URL"+ driver.getCurrentUrl() );
+		//driver.navigate().back();
 		//driver.navigate().forward();
 		System.out.println("Page Title-" + driver.getTitle());
 		//driver.navigate().to("https://the-internet.herokuapp.com/login");
@@ -31,10 +44,10 @@ public class GooglePageTest {
 		//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
 		//driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
 		//driver.findElement(By.xpath("//input[@name='password']")).sendKeys("SuperSecretPassword!");
-		//driver.findElement(By.className("radius")).click();
-		//driver.findElement(By.xpath("//button[@class='radius']")).click();
+//		driver.findElement(By.className("radius")).click();
+//		driver.findElement(By.xpath("//button[@class='radius']")).click();
 		
-		//driver.close();
+		driver.close();
 		
 	}
 
